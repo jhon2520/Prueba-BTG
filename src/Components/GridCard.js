f9b393d05032d7fa2dc59c356fa95a767ccf66a2
jhon2520 <63693418+jhon2520@users.noreply.github.com>
2022-03-21 12:56:23 -0500

    pagina de descripcion del fondo terminada

--- src/Components/GridCard.js
+++ src/Components/GridCard.js
@@ -1,26 +1,13 @@
-import React,{useEffect, useState} from 'react'
+import React from 'react'
 import {Link} from "react-router-dom"
 import styles from "../CSS/GridCard.module.css"
 
 export default function GridCard(props) {
-
-    const [validacion, setValidacion] = useState(false)
     
-
-//TODO:Esta validacion la debo hacer desde el grid y no desde aquí.
-
-    useEffect(()=>{
-        const valorMinimo = props.monto
-        const capital = localStorage.getItem("capital");
-        const validacion = valorMinimo <= capital
-        setValidacion(validacion);
-
-    },[])
 
     return (
         <>
             <div className={styles.card}>
-                <div className={validacion === true ? styles.indicador_verde :styles.indicador_rojo}></div>
                 <h3>{props.nombre}</h3>
                 <hr/>
                 <p>Consectetur ex dolor <span className={styles.span}> mollit non consectetur </span>deserunt. Aliqua proident elit aliqua eu adipisicing occaecat consequat mollit eu dolor ad excepteur tempor.</p>
--- src/Pages/InfoPage.js
+++ src/Pages/InfoPage.js
@@ -1,11 +1,47 @@
 import React from 'react'
-import {useParams} from "react-router-dom"
+import {useParams,useNavigate} from "react-router-dom"
+import styles from "../CSS/InfoPage.module.css"
+import InitialData from '../Data/InitialData';
 
 export default function InfoPage() {
 
     const params = useParams();
+    const navigate = useNavigate();
+    const capital = localStorage.getItem("capital")
+
+    const montoMinimo = InitialData.filter((e)=> e.nombre === params.nombreFondo)[0].monto 
+    const tipoFondo = InitialData.filter((e)=>e.nombre === params.nombreFondo)[0].categoria
+    const validacion = capital >=montoMinimo;
+
+    const handleClick = () =>{
+        navigate("/");
+    }
+
 
     return (
-        <div>{params.nombreFondo}</div>
+        <div className={styles.container}>
+                <img className={styles.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Btg-logo-blue.svg/1200px-Btg-logo-blue.svg.png" alt="" />
+            <section>
+                <article className={styles.article}>
+                    <h2>{params.nombreFondo}</h2>
+                    <p>Dolor exercitation consectetur laboris est dolore Lorem tempor qui veniam. Reprehenderit anim reprehenderit in aliquip adipisicing voluptate exercitation consequat.aliquip adipisicing voluptate exercitation consequat consequat</p>
+                    <hgroup className={styles.valores_fondo}>
+
+                        <h3 className={styles.categoria}>Categoria del fondo: <span className={styles.categoria_valor} >{tipoFondo}</span></h3>
+
+                        <h3 className={styles.categoria}>Monto mínimo: <span className={styles.categoria_valor} >
+                        {montoMinimo.toLocaleString("es-ES",{style:"currency",currency:"COP", minimumFractionDigits:0})}</span></h3>
+
+                    </hgroup>
+                    
+                    {
+                        validacion ? <h3 className={styles.capital_disponible}>Actualmente cuenta con capital para subscribirse a este fondo</h3>:<h3 className={styles.capital_no_disponible}>Actualmente no cuenta con capital para subscribirse a este fondo</h3>
+                    }
+
+                    <button className={styles.btn} onClick={handleClick}>Volver</button>
+                </article>
+                <img src="https://www.dreamhost.com/blog/wp-content/uploads/2018/09/elements-web-design-opt-750x498.jpg" alt="" />
+            </section>
+        </div>
     )
 }
